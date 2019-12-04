e7ff477e70369fc80512dbf0852d876f148c3864
jcobb <jakubgol2020@gmail.com>
2019-12-04 01:41:42 +0100

    learning works not :(

--- network/neural_network.cpp
+++ network/neural_network.cpp
@@ -2,16 +2,16 @@
 #include <iostream>
 #include <math.h>
 
-const int image_height = 30;
-const int image_width = 30;
+const int image_height = 2;
+const int image_width = 1;
 const int entry_layer_neurons = image_width * image_height;
-const int hidden_layer_neurons = 16;
+const int hidden_layer_neurons = 2;
 const int layers_number = 3;
 
 
-const float learning_const = 0.2;
+const float learning_const = 0.1;
 
-const int classes = 4; // exit layer neurons
+const int classes = 2; // exit layer neurons
 
 
 struct neuron {
@@ -30,15 +30,16 @@ struct network {
 };
 
 float sigmoid(float x) {
-    return x / (1 + exp(-x));
+    return 1 / (1 + exp(-x));
 }
 
 float transfer_derivative(float x) {
-    return x * (1 - x);
+    float output =  x * (1.0 - x);
+    return output;
 }
 
 float generate_random_weight() {
-    return (rand() % 100) / 100.0;
+    return ((rand() % 200) / 100.0) - 1.0 ;
 }
 
 network initialize() {
@@ -101,6 +102,8 @@ network initialize() {
         new_network.layers[2].neurons[i].activation = 0;
         for (int j = 0; j < hidden_layer_neurons; j++) {
             new_network.layers[2].neurons[i].weights[j] = generate_random_weight();
+//            new_network.layers[2].neurons[i].weights[j] = 0.0;
+
         }
         new_network.layers[2].neurons[i].bias = generate_random_weight();
     }
@@ -141,12 +144,12 @@ void calculate_neuron_activation(network &our_network, int layer_number, neuron 
         activation += neuron1.weights[i] * our_network.layers[layer_number - 1].neurons[i].activation;
     }
     activation += neuron1.bias;
-    activation = activation / (previous_layer_neurons + 1);
+//    activation = activation / (previous_layer_neurons + 1);
     activation = sigmoid(activation);
     neuron1.activation = activation;
 }
 
-void calculate_output(network our_network) {
+void calculate_output(network &our_network) {
     for (int i = 0; i < classes; i++) {
         calculate_neuron_activation(our_network, 2, our_network.layers[2].neurons[i]);
     }
@@ -197,16 +200,26 @@ void backpropagate(network &our_network, int expected_class) {
     //Update weights
     for (int i=0; i<classes; i++){
         neuron& updated_neuron = our_network.layers[2].neurons[i];
+        float change = 0;
+        float old = 0;
+
         for(int j=0; j< hidden_layer_neurons; j++){
-            updated_neuron.weights[j] = updated_neuron.weights[j] + exit_layer_errors[i] * updated_neuron.activation * learning_const;
+            change = updated_neuron.weights[j] + exit_layer_errors[i] * updated_neuron.activation * learning_const;
+            old = updated_neuron.weights[j];
+            updated_neuron.weights[j] = change ;
         }
+        updated_neuron.bias = updated_neuron.bias + exit_layer_errors[i] * updated_neuron.activation * learning_const;
     }
 
     for (int i=0; i<hidden_layer_neurons; i++){
         neuron& updated_neuron = our_network.layers[1].neurons[i];
+        float change = 0;
         for(int j=0; j<entry_layer_neurons; j++){
-            updated_neuron.weights[j] = updated_neuron.weights[j] + hidden_layer_errors[i] * updated_neuron.activation * learning_const;
+            change = updated_neuron.weights[j] + hidden_layer_errors[i] * updated_neuron.activation * learning_const;
+            updated_neuron.weights[j]=change;
         }
+        updated_neuron.bias = updated_neuron.bias + hidden_layer_errors[i] * updated_neuron.activation * learning_const;
+
     }
 }
 
@@ -223,18 +236,20 @@ void load_image(float pixels[entry_layer_neurons], network &our_network){
 }
 
 
-void learn(float pixels[entry_layer_neurons], network &our_network, int class_number){
+void learn(float pixels[entry_layer_neurons], network &our_network, int class_number, bool write){
     load_image(pixels, our_network);
     for (int i=0; i<hidden_layer_neurons; i++){
         calculate_neuron_activation(our_network, 1, our_network.layers[1].neurons[i]);
     }
-    for (int i=0; i<hidden_layer_neurons; i++){
-        std::cout<<our_network.layers[1].neurons[i].activation<<std::endl;
-    }
+//    for (int i=0; i<hidden_layer_neurons; i++){
+////        std::cout<<our_network.layers[1].neurons[i].activation<<std::endl;
+//    }
 
     calculate_output(our_network);
     int result = choose_result(our_network);
-    std::cout<<"wanted "<<class_number<<"got "<< result<<std::endl;
+    if(write) {
+        std::cout << "wanted " << class_number << "got " << result << std::endl;
+    }
     backpropagate(our_network, class_number);
 
 }
@@ -242,16 +257,40 @@ void learn(float pixels[entry_layer_neurons], network &our_network, int class_nu
 int main() {
     srand( time( 0 ) );
     float random_arr[entry_layer_neurons];
+    float random_arr2[entry_layer_neurons];
+    float random_arr3[entry_layer_neurons];
+
     for (int i=0; i<entry_layer_neurons; i++){
-        random_arr[i] = rand()%100 /100.0;
+//        random_arr[i] = ((rand()%50)/100.0 + 0.5);
+//        random_arr2[i] = rand()%100 /100.0;
+//        random_arr3[i] = rand()%100 /100.0;
+
     }
-
-
-
+    float false1[] = {0,0};
+    float false2[] = {1,1};
+    float true1[] = {0,1};
+    float true2[] = {1,0};
     network our_network = initialize();
-    for(int i=0; i<1000; i++){
-        learn(random_arr, our_network, 1);
+//    learn(random_arr, our_network, 0, true);
+//    learn(random_arr3, our_network, 1, true);
+//    learn(random_arr2, our_network, 2, true);
+
+    for(int i=0; i<10000; i++){
+//        learn(random_arr, our_network, 1, false);
+//        learn(random_arr3, our_network, 3, false);
+//        learn(random_arr2, our_network, 2, false);
+        learn(false1, our_network, 0, true);
+        learn(false2, our_network, 0, true);
+        learn(true1, our_network, 1, true);
+        learn(true2, our_network, 1, true);
     }
+    learn(false1, our_network, 0, true);
+    learn(false2, our_network, 0, true);
+    learn(true1, our_network, 1, true);
+    learn(true2, our_network, 1, true);
+//    learn(random_arr, our_network, 1, true);
+//    learn(random_arr3, our_network, 3, true);
+//    learn(random_arr2, our_network, 2, true);
 //    display_network(our_network);
 
     return 0;
